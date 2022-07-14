051d38566ea10e8ac01e9188485c41665a4361f9
Doan Anh  Tien <d.atien228@gmail.com>
2022-07-15 01:20:09 +0700

    init homepage, remove redudant fonts

---
+++ static/js/style.js
@@ -0,0 +1,8 @@
+// function fadeSeq(baseID) {
+//     var elems = $(baseID).children();
+//     $(elems).each(function(index) {
+//         $(this).delay(1200*index).fadeIn(1000);
+//     });
+// };
+
+// fadeSeq('#biography');

c4d5d8e6f6fc9451f594f6dc2359f6fef5593fa4
Doan Anh  Tien <d.atien228@gmail.com>
2022-07-17 04:35:53 +0700

    add experience and project pages

--- static/js/style.js
+++ static/js/style.js
@@ -1,18 +1,30 @@
 var swiper = new Swiper(".mySwiper", {
-  effect: "cards",
-  cardsEffect: {
-    rotate: false,
-    slideShadows: false
-  },
+//   effect: "cards",
+//   cardsEffect: {
+//     rotate: true,
+//     slideShadows: false
+//   },
   loop: true,
-  grabCursor: true,
+//   grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
     pageUpDown: true
   },
+});
+
+$(document).ready(function () {
+    var slide = function () {
+        id_val = $('.swiper-slide-active').attr('id');
+        $('#resume').find('h2').text(id_val)
+    };
+    setInterval(slide, 100);
 });
